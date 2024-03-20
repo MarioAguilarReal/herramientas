77023c38613c43e4eb8671f55249a05ae4dbe4be
FerUgalde <fernanda.ugalde@uabc.edu.mx>
2024-03-20 16:08:19 -0700

    zoo map

--- resources/views/zones/edit.blade.php
+++ resources/views/zones/edit.blade.php
@@ -1,19 +1,19 @@
 @extends('index')
 @section('content')
     <div class="container">
-        <h1>Editar zona</h1>
+        <h1>Edit Zone</h1>
         <form action="{{ route('zones.update', $zone->id) }}" method="POST">
             @csrf
             @method('PUT')
             <div class="form-group">
-                <label for="name">Nombre</label>
+                <label for="name">Name</label>
                 <input type="text" class="form-control" id="name" name="name" value="{{ $zone->name }}">
             </div>
             <div class="form-group">
-                <label for="description">Descripción</label>
+                <label for="description">Description</label>
                 <textarea class="form-control" id="description" name="description" rows="3">{{ $zone->description }}</textarea>
             </div>
-            <button type="submit" class="btn btn-primary">Actualizar</button>
+            <button type="submit" class="btn btn-primary">Update</button>
         </form>
     </div>
 @endsection
